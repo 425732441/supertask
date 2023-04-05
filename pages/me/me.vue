f6822d4a2ef876356757f4236dbf68e2e834e18c
hualeizhang <hualeizhang@Emotibot.com>
2023-04-05 22:24:15 +0800

    me

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,22 +1,216 @@
 <template>
 	<view>
-		me
+		<uni-section title="我的..." type="line" sub-title="通过日历看板对每天的任务进行查阅" padding>
+			<uni-grid :column="3" :show-border="false" :square="false" @change="change">
+				<uni-grid-item v-for="(item ,index) in list" :index="index" :key="index">
+					<view class="grid-item-box">
+						<uni-icons :type="item.icon" size="50"></uni-icons>
+						<image class="image" :src="item.url" mode="aspectFill" />
+						<text class="text">{{item.text}}</text>
+						<view v-if="item.badge" class="grid-dot">
+							<uni-badge :text="item.badge" :type="item.type" />
+						</view>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+
+
+
+			<uni-popup ref="popup" type="bottom">
+				<view class="popup">
+					<uni-calendar ref="calendar" :insert="true" :range="calendar.range" class="uni-calendar--hook"
+						:selected="calendar.selected" :showMonth="true" @change="changeDate"
+						@monthSwitch="monthSwitch" />
+				</view>
+			</uni-popup>
+		</uni-section>
 	</view>
 </template>
 
 <script>
+	/**
+	 * 获取任意时间
+	 */
+	function getDate(date, AddDayCount = 0) {
+		if (!date) {
+			date = new Date()
+		}
+		if (typeof date !== 'object') {
+			date = date.replace(/-/g, '/')
+		}
+		const dd = new Date(date)
+
+		dd.setDate(dd.getDate() + AddDayCount) // 获取AddDayCount天后的日期
+
+		const y = dd.getFullYear()
+		const m = dd.getMonth() + 1 < 10 ? '0' + (dd.getMonth() + 1) : dd.getMonth() + 1 // 获取当前月份的日期，不足10补0
+		const d = dd.getDate() < 10 ? '0' + dd.getDate() : dd.getDate() // 获取当前几号，不足10补0
+		return {
+			fullDate: y + '-' + m + '-' + d,
+			year: y,
+			month: m,
+			date: d,
+			day: dd.getDay()
+		}
+	}
 	export default {
 		data() {
 			return {
-				
+				calendar: {
+					range: false,
+					selected: [{
+							date: getDate(new Date(), -3).fullDate,
+							info: 'AAaaaabbbbbbbbbaaa',
+							qqq: 'qqq'
+						},
+						{
+							date: getDate(new Date(), -2).fullDate,
+							info: 'BBB',
+							data: {
+								custom: '自定义信息',
+								name: '自定义消息头'
+							}
+						},
+						{
+							date: getDate(new Date(), -1).fullDate,
+							info: '已打卡'
+						}
+					]
+				},
+				list: [{
+						url: '/static/c1.png',
+						text: '看板',
+						badge: '0',
+						icon: "calendar"
+					},
+					{
+						url: '/static/c2.png',
+						text: 'Grid 2',
+						badge: '1',
+						type: "success"
+					},
+					{
+						url: '/static/c3.png',
+						text: 'Grid 3',
+						badge: '99',
+						type: "warning"
+					},
+					{
+						url: '/static/c4.png',
+						text: 'Grid 4',
+						badge: '2',
+						type: "error"
+					},
+					{
+						url: '/static/c5.png',
+						text: 'Grid 5'
+					},
+					{
+						url: '/static/c6.png',
+						text: 'Grid 6'
+					},
+					{
+						url: '/static/c7.png',
+						text: 'Grid 7'
+					},
+					{
+						url: '/static/c8.png',
+						text: 'Grid 8'
+					},
+					{
+						url: '/static/c9.png',
+						text: 'Grid 9'
+					}
+				]
+
 			}
 		},
 		methods: {
-			
+			change(e) {
+				this.$refs.popup.open();
+				console.log(e);
+			},
+			changeDate(e) {
+				console.log(e);
+			},
+			monthSwitch(e) {
+
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.popup {
+		margin-bottom: var(--window-bottom);
+	}
 
+	.image {
+		width: 25px;
+		height: 25px;
+	}
+
+	.text {
+		font-size: 14px;
+		margin-top: 5px;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		// display: block;
+		/* #endif */
+	}
+
+	.grid-dynamic-box {
+		margin-bottom: 15px;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-item-box-row {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-dot {
+		position: absolute;
+		top: 5px;
+		right: 15px;
+	}
+
+	.swiper {
+		height: 420px;
+	}
+
+	/* #ifdef H5 */
+	@media screen and (min-width: 768px) and (max-width: 1425px) {
+		.swiper {
+			height: 630px;
+		}
+	}
+
+	@media screen and (min-width: 1425px) {
+		.swiper {
+			height: 830px;
+		}
+	}
+
+	/* #endif */
 </style>
